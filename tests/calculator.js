a8ec5df430bd23ad90209ae8f539a1ee36dc6ee6
Atifkamal259 <atif.kamal@bestercapitalmedia.com>
2021-01-26 10:25:34 +0500

    my new commit

--- tests/calculator.js
+++ tests/calculator.js
@@ -17,23 +17,6 @@ describe('demo calculator tests', function () {
         browser.sleep(3000)
 
     });
-
-    it('subtraction test', function () {
-
-        homepage.get('http://juliemr.github.io/protractor-demo/');
-
-        homepage.enterFirstNumber('4');
-
-        homepage.enterSecondNumber('3');
-
-        homepage.clickGo1();
-
-        homepage.verifyResult('7');
-
-        browser.sleep(2000)
-
-    });
-
    
 
 });
